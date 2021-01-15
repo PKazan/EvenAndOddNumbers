9abeab52183b91f693af85f4c6777b199f2dad2d
Paweł Kazanecki <kazan87@gmail.com>
2021-01-15 23:38:11 +0100

    Added tests for each method

---
+++ src/test/java/EvenAndOddNumbersTest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class EvenAndOddNumbersTest {
+
+@Test
+    void is5even() {
+    boolean result = EvenAndOddNumbers.isEvenNumber(5);
+
+    Assertions.assertFalse(result);
+}
+    @Test
+    void is6even() {
+        boolean result = EvenAndOddNumbers.isEvenNumber(6);
+
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    void is5odd() {
+    boolean result = EvenAndOddNumbers.isOddNumber(5);
+
+    Assertions.assertTrue(result);
+    }
+    @Test
+    void is6odd() {
+        boolean result = EvenAndOddNumbers.isOddNumber(6);
+
+        Assertions.assertFalse(result);
+    }
+
+
+
+}
+

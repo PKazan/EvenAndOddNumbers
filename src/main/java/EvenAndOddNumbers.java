e71f81f5b5eb34627fcc6ef7ba776964a797504f
Paweł Kazanecki <kazan87@gmail.com>
2021-01-15 23:22:57 +0100

    Added methods to verify if number is even or odd

--- src/main/java/EvenAndOddNumbers.java
+++ src/main/java/EvenAndOddNumbers.java
@@ -1,5 +1,22 @@
 public class EvenAndOddNumbers {
     public static void main(String[] args) {
+        System.out.println(isEvenNumber(11));
+        System.out.println(isOddNumber(11));
+    }
 
+    public static boolean isEvenNumber(int number) {
+        if (number % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean isOddNumber(int number) {
+        if (number % 2 != 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
